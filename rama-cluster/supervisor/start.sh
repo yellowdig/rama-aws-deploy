25de11bdb0a41de778ebf59e686cfb89bd3db4f4
Brian Hurlow <brian@brianhurlow.com>
2024-04-05 16:03:01 -0400

    disable public_ip setup script

--- rama-cluster/supervisor/start.sh
+++ rama-cluster/supervisor/start.sh
@@ -4,12 +4,15 @@ mv /run/rama/rama.yaml /data/rama/rama.yaml
 
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 PRIVATE_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
-PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+
+# when deployed to YD private network, request for PUBLIC_IP returns 404 which breaks rama boot sequence
+# PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+# we're ok with private in all cases due to VPN
 
 cat <<EOF >> /data/rama/rama.yaml
 
 supervisor.host:
-  external: "$PUBLIC_IP"
+  external: ""
   internal: "$PRIVATE_IP"
 EOF
 
